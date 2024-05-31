3fda4cd8bc3bcc45a5ed8965f4fd0bbfdf11ac3b
stekap000 <stekap000@gmail.com>
2024-06-01 01:10:00 +0200

    minkowski sum and diff

--- examples/minkowski_sum_and_diff.c
+++ examples/minkowski_sum_and_diff.c
@@ -1,12 +1,12 @@
 #define DGL_TARGET_WINDOWS
 #define DGL_USE_ENGINE
-#define DGL_COORDINATE_CENTER_X 250
-#define DGL_COORDINATE_CENTER_Y 250
+#define DGL_COORDINATE_CENTER_X 350
+#define DGL_COORDINATE_CENTER_Y 350
 #define DGL_IMPLEMENTATION
 #include "dgl.h"
 
-int width = 500;
-int height = 500;
+int width = 700;
+int height = 700;
 
 dgl_Point2D v3_to_point2d(dgl_V3 v) {
 	return (dgl_Point2D){ .x = v.x, .y = v.y };
@@ -19,7 +19,9 @@ typedef struct {
 
 convex_shape shape1;
 convex_shape shape2;
+convex_shape shape2_inverted;
 convex_shape minkowski_sum = {0};
+convex_shape minkowski_diff = {0};
 
 void scale_shape(convex_shape *shape, float f) {
 	for(int i = 0; i < shape->n; ++i) dgl_v3_scale_mut(&shape->vertices[i], f);
@@ -27,6 +29,14 @@ void scale_shape(convex_shape *shape, float f) {
 
 void translate_shape(convex_shape *shape, dgl_V3 v) {
 	for(int i = 0; i < shape->n; ++i) dgl_v3_add_mut(&shape->vertices[i], v);
+}
+
+convex_shape deep_copy_shape(convex_shape shape) {
+	convex_shape temp = {0};
+	temp.n = shape.n;
+	temp.vertices = malloc(temp.n * sizeof(dgl_V3));
+	for(int i = 0; i < temp.n; ++i) temp.vertices[i] = shape.vertices[i];
+	return temp;
 }
 
 void draw_convex_shape(convex_shape shape, dgl_Color color) {
@@ -68,8 +78,8 @@ void start() {
 	shape1.vertices = malloc(shape1.n * sizeof(dgl_V3));
 	shape2.vertices = malloc(shape2.n * sizeof(dgl_V3));
 
-	shape1.vertices[0] = (dgl_V3){ .x = -30, .y = 0, .z = 0 };
-	shape1.vertices[1] = (dgl_V3){ .x = 30, .y = 0, .z = 0 };
+	shape1.vertices[0] = (dgl_V3){ .x = -20, .y = 0, .z = 0 };
+	shape1.vertices[1] = (dgl_V3){ .x = 20, .y = 0, .z = 0 };
 	shape1.vertices[2] = (dgl_V3){ .x = 0, .y = 40, .z = 0 };
 
 	shape2.vertices[0] = (dgl_V3){ .x = -20, .y = 20, .z = 0 };
@@ -79,27 +89,32 @@ void start() {
 
 	scale_shape(&shape1, 3);
 	scale_shape(&shape2, 3);
+
+	translate_shape(&shape2, (dgl_V3){120, 120, 0});
+	
+	shape2_inverted = deep_copy_shape(shape2);
+	scale_shape(&shape2_inverted, -1);
 	
 	minkowski_sum_shape_alloc(&minkowski_sum, shape1, shape2);
+	minkowski_sum_shape_alloc(&minkowski_diff, shape1, shape2_inverted);
 }
 
-convex_shape temp;
 void update(float dt) {
 	dgl_clear(&window.canvas, DGL_BLACK);
 	dgl_draw_vertical_line(&window.canvas, 0, DGL_RGBA(255, 255, 255, 120));
 	dgl_draw_horizontal_line(&window.canvas, 0, DGL_RGBA(255, 255, 255, 120));
-	dgl_fill_circle(&window.canvas, cursor_pos.x, cursor_pos.y, 3, DGL_YELLOW);
+	dgl_fill_circle(&window.canvas, cursor_pos.x, cursor_pos.y, 5, DGL_YELLOW);
 	
-	temp = shape1;
+	translate_shape(&shape1, (dgl_V3){ cursor_pos.x, cursor_pos.y, 0});
 	
-	translate_shape(&temp, (dgl_V3){ cursor_pos.x, cursor_pos.y, 0});
-	
-	draw_convex_shape(temp, DGL_RGBA(255, 0, 0, 120));
-	draw_convex_shape(shape2, DGL_RGBA(0, 255, 0, 120));
+	draw_convex_shape(shape1, DGL_WHITE);
 	minkowski_sum_shape_update(&minkowski_sum, shape1, shape2);
-	draw_convex_shape(minkowski_sum, DGL_RGBA(0, 0, 255, 120));
+	minkowski_sum_shape_update(&minkowski_diff, shape1, shape2_inverted);
+	draw_convex_shape(minkowski_sum, DGL_RGBA(0, 0, 255, 80));
+	draw_convex_shape(minkowski_diff, DGL_RGBA(255, 255, 0, 80));
+	draw_convex_shape(shape2, DGL_GREEN);
 	
-	translate_shape(&temp, (dgl_V3){ -cursor_pos.x, -cursor_pos.y, 0});
+	translate_shape(&shape1, (dgl_V3){ -cursor_pos.x, -cursor_pos.y, 0});
 }
 
 void end() {}
